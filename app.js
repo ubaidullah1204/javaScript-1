f561afd4e5261db642bb870862c400e58a4ff978
ubaidullah1204 <ubaidkh451@gmail.com>
2024-12-17 20:26:44 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+
+// <!-- <script>
+
+// // example 1
+
+// // question 1 
+
+//    alert(" hello")
+
+// // question 2 
+
+//    alert(" Error! please enter a valid password ")
+
+
+//    // question 3
+
+//    alert(" welcome to js \n land")
+
+// // question 4 
+
+
+// alert("Welcome to JS Land");
+// alert("Happy Coding");
+
+
+// // question 5 
+// console.log("Hello..I can run JS through my web browsers console");
+
+
+
+
+
+// //chapter 2
+
+// // question 1 
+// var myName= ("ubaid");
+// alert(myName);
+
+
+// //question 2 
+
+// var massege= ("hi dear friends");
+// alert(massege);
+
+// //question 3 
+
+
+// var biodata= (" 1 may 2003");
+// alert(biodata);
+
+// //question 4 
+
+// var pizza= ("pizza\npizz\npiz\npi\np");
+// alert(pizza);
+
+// //question 5
+
+// var email= ("ubaidkh@gmail.com");
+// alert(email);
+
+
+// //question 6
+
+
+// document.write("<h1>Thanks for watching</h1>")        
+
+// </script> -->
